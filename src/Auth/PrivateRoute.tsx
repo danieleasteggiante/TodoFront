5bd9db74eceb3972a6d0c80e3b75a83dd09d1e83
WRK0151\daniele.asteggiante <daniele.asteggiante@netservice.eu>
2024-02-22 23:57:27 +0100

    added login authContext privateRoute e CallApi

--- src/Auth/PrivateRoute.tsx
+++ src/Auth/PrivateRoute.tsx
@@ -1,11 +1,15 @@
 import { Navigate, useLocation } from "react-router";
 import {useAuth} from "./AuthContext";
 
-export const PrivateRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
+interface PrivateRouteProps {
+    children: JSX.Element;
+}
+
+export function PrivateRoute ({ children }: PrivateRouteProps) {
     let auth = useAuth();
     let location = useLocation();
-
-    if (!auth.isAuthenticated) return <Navigate to="/" state={{ from: location }} />;
+    if (!auth.isAuthenticated)
+        return <Navigate to="/" state={{ from: location }} />;
 
     return children;
 };
--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,13 +1,56 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import {useAuth} from "../Auth/AuthContext";
+import {callApi, ICallApi} from "../Calls/CallApi";
+import Constant from '../Constant/Constant';
+import {useNavigate} from "react-router-dom";
+interface IAccessProps {
+    username?: string;
+    password?: string;
+}
 function Login(){
+    const [apiData, setApiData] = useState<IAccessProps>();
+    const authContext = useAuth();
+    const navigate = useNavigate();
+    function handleLogin(event: React.MouseEvent<HTMLButtonElement>){
+        event.preventDefault();
+        if (!apiData) return;
+        callApi({
+                url: Constant.API_URL + Constant.API_LOGIN,
+                method: "POST",
+                body: JSON.stringify(apiData),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then((response: Response | undefined) => {
+                if (!response)
+                    throw new Error("No response");
+                if (response.ok)
+                    authContext.login();
+                if (authContext.isAuthenticated)
+                    navigate("/dashboard");
+            }).catch((error: any) => {
+                console.error(error);
+            });
+    }
+    function handleUsernameChange(event: React.ChangeEvent<HTMLInputElement>){
+        setApiData({
+            ...apiData,
+            username: event.target.value
+        });
+    }
+    function handlePasswordChange(event: React.ChangeEvent<HTMLInputElement>){
+        setApiData({
+            ...apiData,
+            password: event.target.value
+        });
+    }
     return (
         <div>
             <h1>Login</h1>
             <form>
-                <input type="text" placeholder="Username"/>
-                <input type="password" placeholder="Password"/>
-                <button type="submit">Login</button>
+                <input type="text" onChange={handleUsernameChange} placeholder="Username"/>
+                <input type="password" onChange={handlePasswordChange} placeholder="Password"/>
+                <button type="submit" onClick={handleLogin}>Login</button>
             </form>
         </div>
     )
